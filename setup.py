8f12cc837dec1cbeddfce3b832dbd47202b7d675
Connor Kapahi <13ck15@queensu.ca>
2016-06-15 13:24:22 -0400

    Renamed Classes and modules for clarity Objects and modules have been renamed to prevent confusion for the user. Module imports have been set for internal testing, this release is not intented for publication on PyPI.

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from distutils.core import setup
 setup(
-    name='QExPy',
-    packages=['QExPy'],
-    version='0.1.2',
+    name='qexpy',
+    packages=['qexpy'],
+    version='0.0.1',
     description='''Objects to handle calculations with uncertainty and
     plotting with a focus on use in physics.''',
     author='Connor Kapahi and Prof. Ryan Martin',
     author_email='connorkapahi@gmail.com',
     license='GNU GLP v3',
     url='https://github.com/Queens-Physics/qphyssy',
-    download_url='https://github.com/Queens-Physics/qphyssy/tarball/0.1',
+    download_url='https://github.com/Queens-Physics/QExPy/tarball/0.2',
     keywords=['physics', 'undergrad', 'queens', 'university', 'analysis',
                 'uncertainty', 'plotting', 'error analysis'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Physics',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU GLP v3',
         'Programming Language :: Python',
     ],
     install_requires=['numpy', 'scipy'],
