cf1fb2292282937f495679d3823ee9bdfedbf1d8
ryanmartinneutrino <ryanmartinneutrino@gmail.com>
2016-08-25 11:10:51 -0400

    cleaned up, reverted ExperimentalValue constructor, added all public stuff to qexpy init

--- qexpy/__init__.py
+++ qexpy/__init__.py
@@ -1,10 +1,11 @@
 __version__ = '0.2.4'
 
-from qexpy.error import Measurement, MeasurementArray
+from qexpy.error import Measurement, MeasurementArray,\
+                        set_print_style, set_sigfigs_centralvalue, set_sigfigs_error, set_sigfigs, set_error_method,\
+                        sqrt, sin, cos, tan, sec, csc, cot, log, exp, e, asin, acos, atan
+        
 from qexpy.plotting import Plot
 import qexpy.utils as qu
-
-
 
 import bokeh.plotting as bp
 import bokeh.io as bi
