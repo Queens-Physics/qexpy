3271b9f3939d11d170542bbbff792752c76335e5
Turner <tugarrow15@gmail.com>
2017-05-23 14:24:52 -0400

    Testing updating docs.

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ import sphinx_rtd_theme
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
+os.environ['BOKEH_DOCS_MISSING_API_KEY_OK'] = str(1)
 
 # -- General configuration ------------------------------------------------
 
@@ -88,7 +89,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', '**.ipynb_checkpoints']
+exclude_patterns = ['_build', '**.ipynb_checkpoints', 'conf.py']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
