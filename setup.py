897db0af26ebf90a6e47935c21a4b9abfc1e92b1
Ryan Martin <ryanmartinneutrino@gmail.com>
2018-01-04 14:22:51 -0500

    updated tags to deal with bug fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='qexpy',
     packages=['qexpy'],
-    version='1.0.0',
+    version='1.0.1',
     description='''Package to handle error analysis and data plotting aimed
     at undergraduate physics.''',
     author='Connor Kapahi and Prof. Ryan Martin',
