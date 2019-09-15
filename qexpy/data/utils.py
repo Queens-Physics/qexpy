b7db09b4305a0181f79c98e3390c7164eb6e9525
Astral Cai <astralcai@gmail.com>
2019-09-14 23:57:04 -0400

    Refactor error propagation for the Monte Carlo method
    Implement the Evaluator class and the MonteCarloSetting class to
    enable easier customization of the Monte Carlo error propagation
    process, as well as increase robustness of the code structure.

--- qexpy/data/utils.py
+++ qexpy/data/utils.py
@@ -1,4 +1,5 @@
 """Utility methods for the data module"""
+import warnings
 
 import numpy as np
 
@@ -6,7 +7,125 @@ from numbers import Real
 
 from . import data as dt, datasets as dts  # pylint: disable=cyclic-import
 
+import qexpy.settings.literals as lit
+import qexpy.settings as sts
+
 ARRAY_TYPES = np.ndarray, list
+
+
+class MonteCarloSettings:
+    """The object for customizing the Monte Carlo error propagation process"""
+
+    def __init__(self, evaluator):
+        self.__evaluator = evaluator
+        self.__settings = {
+            lit.MONTE_CARLO_SAMPLE_SIZE: 0,
+            lit.MONTE_CARLO_STRATEGY: lit.MC_MEAN_AND_STD,
+            lit.MONTE_CARLO_CONFIDENCE: 0.68
+        }
+
+    @property
+    def sample_size(self) -> int:
+        """int: The Monte Carlo sample size"""
+        default_size = sts.get_settings().monte_carlo_sample_size
+        set_size = self.__settings[lit.MONTE_CARLO_SAMPLE_SIZE]
+        return set_size if set_size else default_size
+
+    @sample_size.setter
+    def sample_size(self, new_size: int):
+        if not isinstance(new_size, int) or new_size < 0:
+            raise ValueError("The sample size has to be a positive integer")
+        self.__settings[lit.MONTE_CARLO_SAMPLE_SIZE] = new_size
+        self.__evaluator.clear()
+
+    @property
+    def confidence(self):
+        """float: The confidence level for choosing the mode of a Monte Carlo distribution"""
+        return self.__settings[lit.MONTE_CARLO_CONFIDENCE]
+
+    @confidence.setter
+    def confidence(self, new_level: float):
+        if not isinstance(new_level, float) or new_level > 1 or new_level < 0:
+            raise ValueError("The MC confidence level has to be a number between 0 and 1")
+        self.__settings[lit.MONTE_CARLO_CONFIDENCE] = new_level
+
+    def use_mode_with_confidence(self):
+        """Sets the strategy to mode and uncertainty from confidence coverage"""
+        self.strategy = lit.MC_MODE_AND_CONFIDENCE
+
+    def use_mean_and_std(self):
+        """Sets the strategy to use mean and std of the distribution"""
+        self.strategy = lit.MC_MEAN_AND_STD
+
+    @property
+    def strategy(self):
+        """str: the strategy used to extract value and error from a histogram"""
+        return self.__settings[lit.MONTE_CARLO_STRATEGY]
+
+    @strategy.setter
+    def strategy(self, new_strategy: str):
+        if new_strategy not in [lit.MC_MEAN_AND_STD, lit.MC_MODE_AND_CONFIDENCE]:
+            raise ValueError("Invalid strategy string")
+        self.__settings[lit.MONTE_CARLO_STRATEGY] = new_strategy
+
+
+def generate_offset_matrix(measurements, sample_size):
+    """Generates offsets from mean for each measurement
+
+    Each sample set generated has 0 mean and unit variance. Then covariance is applied to the
+    set of samples using the Chelosky algorithm.
+
+    Args:
+        measurements (List[dt.ExperimentalValue]): a set of measurements to simulate
+        sample_size (int): the size of the samples
+
+    Returns:
+        A N row times M column matrix where N is the number of measurements to simulate and
+        M is the requested sample size for Monte Carlo simulations. Each row of this matrix
+        is an array of random values with 0 mean and unit variance
+
+    """
+
+    offset_matrix = np.vstack(
+        [np.random.normal(0, 1, sample_size) for _ in measurements])
+    offset_matrix = correlate_samples(measurements, offset_matrix)
+    return offset_matrix
+
+
+def correlate_samples(variables, sample_vector):
+    """Uses the Chelosky algorithm to add correlation to random samples
+
+    This method finds the Chelosky decomposition of the correlation matrix of the given list
+    of measurements, then applies it to the sample vector.
+
+    The sample vector is a list of random samples, each entry correspond to each variable
+    passed in. Each random sample, corresponding to each entry, is an array of random numbers
+    with 0 mean and unit variance.
+
+    Args:
+        variables (List[dt.ExperimentalValue]): the source measurements
+        sample_vector (np.ndarray): the list of random samples to apply correlation to
+
+    Returns:
+        The same list sample vector with correlation applied
+
+    """
+
+    corr_matrix = np.array(
+        [[dt.get_correlation(row, col) for col in variables] for row in variables])
+    if np.count_nonzero(corr_matrix - np.diag(np.diagonal(corr_matrix))) == 0:
+        return sample_vector  # if no correlations are present
+
+    try:
+        chelosky_decomposition = np.linalg.cholesky(corr_matrix)
+        result_vector = np.dot(chelosky_decomposition, sample_vector)
+        return result_vector
+    except np.linalg.linalg.LinAlgError:
+        warnings.warn(
+            "Fail to generate a physical correlation matrix for the values provided, using "
+            "uncorrelated samples instead. Please check that the covariance or  correlation "
+            "factors assigned to the measurements are physical.")
+        return sample_vector
 
 
 def wrap_in_experimental_value(operand) -> "dt.ExperimentalValue":
--- qexpy/utils/__init__.py
+++ qexpy/utils/__init__.py
@@ -2,7 +2,8 @@
 
 from .utils import load_data_from_file
 from .utils import vectorize, check_operand_type, validate_xrange, use_mc_sample_size
-from .utils import numerical_derivative, calculate_covariance, cov2corr
+from .utils import numerical_derivative, calculate_covariance, cov2corr, \
+    find_mode_and_uncertainty
 from .exceptions import IllegalArgumentError, UndefinedActionError, UndefinedOperationError
 from .units import parse_units, construct_unit_string, operate_with_units
 from .printing import get_printer
