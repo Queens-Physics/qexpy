06314a91e7365d6f3c830116370eafff96e4023b
Astral Cai <astralcai@gmail.com>
2020-08-10 19:13:25 -0400

    Correct version number in documentation to 2.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Astral Cai, Connor Kapahi, Prof. Ryan Martin'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '3.0.0'
+release = '2.0.0'
 
 # -- General configuration ---------------------------------------------------
 
