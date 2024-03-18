d6973e2b4eda86c01d7b4e7e92d1e15354c39500
Astral Cai <astralcai@gmail.com>
2024-03-18 01:28:15 -0400

    Setup documentation with sphinx and pydata-sphinx-theme

---
+++ docs/source/conf.py
@@ -0,0 +1,97 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+import os
+import re
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+import qexpy
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "QExPy"
+copyright = "2024, Astral Cai"
+author = "Astral Cai"
+release = qexpy.__version__
+version = re.match(r"^(\d+\.\d+)", release).expand(r"\1")
+language = "en"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
+    "sphinx.ext.intersphinx",
+    "nbsphinx",
+    "sphinx_design",
+    "numpydoc",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for Linking -----------------------------------------------------
+
+intersphinx_mapping = {
+    "numpy": ("https://numpy.org/doc/stable", None),
+    "matplotlib": ("https://matplotlib.org/stable", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_logo = "_static/logo-colour.svg"
+html_favicon = "_static/favicon-colour.svg"
+html_static_path = ["_static"]
+html_css_files = ["qexpy.css"]
+html_copy_source = False
+
+html_theme_options = {
+    "navbar_end": ["navbar-icon-links"],
+    "collapse_navigation": True,
+    "navigation_depth": 4,
+    "navbar_align": "left",
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/Queens-Physics/qexpy",
+            "icon": "fa-brands fa-square-github",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Queen's Physics",
+            "url": "https://www.queensu.ca/physics/",
+            "icon": "_static/index-images/queens-logo.png",
+            "type": "local",
+        },
+    ],
+    "primary_sidebar_end": [],
+}
+
+
+html_context = {"default_mode": "light"}
+
+html_sidebars = {"**": ["sidebar-nav-bs.html"]}
+
+
+# -- Options for autodoc -----------------------------------------------------
+
+autosummary_generate = True
+autodoc_typehints = "none"
+
+# -- Options for Numpydoc -----------------------------------------------------
+
+numpydoc_attributes_as_param_list = False
---
+++ docs/source/index.rst
@@ -0,0 +1,95 @@
+.. QExPy documentation master file, created by
+   sphinx-quickstart on Mon Mar 18 00:04:16 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to QExPy documentation!
+=================================
+
+QExPy is a Python package designed to facilitate data analysis in physics laboratories. It provides
+an intuitive interface to record measurements with uncertainties and perform calculations with
+automatic error propagation. QExPy also offers modules for plotting and curve fitting, providing
+researchers with a comprehensive toolkit for data analysis.
+
+.. toctree::
+   :maxdepth: 1
+   :hidden:
+
+   Getting Started <getting-started/index>
+   User Guide <user-guide/index>
+   API Reference <api-reference/index>
+   Development <developer-guide/index>
+
+.. grid:: 2
+
+    .. grid-item-card::
+        :img-top: ./_static/index-images/getting-started.svg
+
+        Getting Started
+        ^^^^^^^^^^^^^^^
+
+        New to QExPy? Check out this beginner's guide. It contains an introduction to the main
+        concepts of QExPy with examples.
+
+        +++
+
+        .. button-ref:: getting-started/index
+            :color: primary
+            :align: center
+            :click-parent:
+
+            To the beginner's guide
+
+    .. grid-item-card::
+        :img-top: ./_static/index-images/user-guide.svg
+
+        User Guide
+        ^^^^^^^^^^
+
+        The user guide provides in-depth information on the core concepts of QExPy and tutorials
+        on how to use different features.
+
+        +++
+
+        .. button-ref:: user-guide/index
+            :color: primary
+            :align: center
+            :click-parent:
+
+            To the user guide
+
+    .. grid-item-card::
+        :img-top: ./_static/index-images/api.svg
+
+        API Reference
+        ^^^^^^^^^^^^^
+
+        The API reference contains a detailed description of the QExPy API. The reference includes
+        the full definition of functions and classes.
+
+        +++
+
+        .. button-ref:: api-reference/index
+            :color: primary
+            :align: center
+            :click-parent:
+
+            To the reference guide
+
+    .. grid-item-card::
+        :img-top: ./_static/index-images/contributor.svg
+
+        Developer Guide
+        ^^^^^^^^^^^^^^^
+
+        Want to make contributions to QExPy? This guide contains an architectural overview and
+        guides you through the process of improving qexpy.
+
+        +++
+
+        .. button-ref:: developer-guide/index
+            :color: primary
+            :align: center
+            :click-parent:
+
+            To the developer's guide
