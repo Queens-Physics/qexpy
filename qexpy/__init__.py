8c9a82b593b18e36bb298e024e30ab97cac803ed
Astral Cai <astralcai@gmail.com>
2025-10-05 21:43:50 -0400

    Implement the Quantity class and Constant class

--- qexpy/__init__.py
+++ qexpy/__init__.py
@@ -32,4 +32,5 @@ import qexpy._config.config_init
 from . import typing
 from .__version__ import __version__
 from ._config import *
+from .core import Constant
 from .units import clear_unit_aliases, define_unit
---
+++ qexpy/core/__init__.py
@@ -0,0 +1,4 @@
+"""Defines the core data structures of qexpy."""
+
+from .constants import Constant, G, c, e, eps0, h, hbar, kb, me, mu0, pi
+from .quantity import Quantity
