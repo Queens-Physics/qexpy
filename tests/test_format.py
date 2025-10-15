58c45d75f26710c4cf774b92a8d3d3068b11acf5
Astral Cai <astralcai@gmail.com>
2025-10-14 23:53:01 -0400

    more tests

--- tests/test_format.py
+++ tests/test_format.py
@@ -7,16 +7,17 @@ from qexpy.format import format_value_error
 
 
 @pytest.mark.parametrize(
-    "value, error, sigfigs, precision_mode, expected",
+    "value, error, sigfigs, expected",
     [
-        (1.23, 0.45, 1, "error", "1.2 +/- 0.4"),
-        (1.23, 0.45, 3, "error", "1.230 +/- 0.450"),
-        (12345, 123, 2, "error", "12340 +/- 120"),
-        (-12.34, 0.0068, 1, "error", "-12.340 +/- 0.007"),
+        (1.23, 0.45, 1, "1.2 +/- 0.4"),
+        (12345, 123, 2, "12340 +/- 120"),
+        (1.23, 0.45, 3, "1.230 +/- 0.450"),
+        (-12.34, 0.0068, 1, "-12.340 +/- 0.007"),
+        (12.2345678, 0.04, 2, "12.235 +/- 0.040"),
     ],
 )
-def test_format_simple(value, error, sigfigs, precision_mode, expected):
-    """Tests constructing a string in the simple format."""
+def test_format_simple_with_error(value, error, sigfigs, expected):
+    """Tests the simple format with error as the precision mode."""
 
     with q.set_option_context(
         "format.value",
@@ -24,6 +25,30 @@ def test_format_simple(value, error, sigfigs, precision_mode, expected):
         "format.precision.sigfigs",
         sigfigs,
         "format.precision.mode",
-        precision_mode,
+        "error",
+    ):
+        assert format_value_error(value, error) == expected
+
+
+@pytest.mark.parametrize(
+    "value, error, sigfigs, expected",
+    [
+        (1.23, 0.45, 2, "1.2 +/- 0.4"),
+        (12345, 123, 3, "12300 +/- 100"),
+        (1.23, 0.45, 4, "1.230 +/- 0.450"),
+        (-12.34, 0.0068, 5, "-12.340 +/- 0.007"),
+        (12.2345678, 0.04, 5, "12.235 +/- 0.040"),
+    ],
+)
+def test_format_simple_with_value(value, error, sigfigs, expected):
+    """Tests the simple format with value as the precision mode."""
+
+    with q.set_option_context(
+        "format.value",
+        "simple",
+        "format.precision.sigfigs",
+        sigfigs,
+        "format.precision.mode",
+        "value",
     ):
         assert format_value_error(value, error) == expected
